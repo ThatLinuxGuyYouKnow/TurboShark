b03ed3b2457ef045bb5049c7c9a788fd9cae2883
Emmanuels-code <arocket04@gmail.com>
2024-12-06 00:49:53 +0100

    fix: synced background colour to colour scheme

--- lib/screens/settingsScreen.dart
+++ lib/screens/settingsScreen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:path_provider/path_provider.dart';
 import 'package:turbo_shark/widgets/appbar.dart';
 
 class SettingsScreen extends StatelessWidget {
@@ -7,6 +9,23 @@ class SettingsScreen extends StatelessWidget {
       backgroundColor: Colors.white,
       appBar: CustomAppbar(
         textContent: 'Settings',
+      ),
+      body: Column(
+        children: [
+          SizedBox(
+            height: 30,
+          ),
+          Row(children: [
+            SizedBox(width: 20),
+            Text(
+              'Default Download PATH',
+              style:
+                  GoogleFonts.ubuntu(fontWeight: FontWeight.w500, fontSize: 20),
+            ),
+            SizedBox(width: 10),
+            Text(getDownloadsDirectory().toString())
+          ])
+        ],
       ),
     );
   }
