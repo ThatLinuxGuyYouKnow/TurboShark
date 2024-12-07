502abffefc6e82385f0b8c9c94c4c619c1a46cec
Emmanuels-code <arocket04@gmail.com>
2024-12-07 22:47:36 +0100

    feature: integrating minimal dark mode support

--- lib/widgets/switches.dart
+++ lib/widgets/switches.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:turbo_shark/user_preferences.dart';
 
 class AutoResumeSwitch extends StatefulWidget {
   const AutoResumeSwitch({super.key});
@@ -34,6 +35,44 @@ class _AutoResumeSwitchState extends State<AutoResumeSwitch> {
           activeColor: Colors.blue,
           inactiveThumbColor: Colors.grey,
           inactiveTrackColor: Colors.grey.withOpacity(0.5),
+        ),
+      ],
+    );
+  }
+}
+
+class darkModeSwitch extends StatefulWidget {
+  @override
+  State<darkModeSwitch> createState() => _darkModeSwitchState();
+}
+
+class _darkModeSwitchState extends State<darkModeSwitch> {
+  final UserPreferences userPreferences = UserPreferences();
+
+  Widget build(BuildContext context) {
+    bool isDarkMode = userPreferences.getTheme();
+
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.start,
+      children: [
+        Text(
+          isDarkMode ? 'Switch to' + 'Dark Mode' : 'Switch to light mode',
+          style: GoogleFonts.ubuntu(fontSize: 18, fontWeight: FontWeight.w600),
+        ),
+        SizedBox(
+          width: 10,
+        ),
+        Switch(
+          value: isDarkMode,
+          onChanged: (value) {
+            setState(() {
+              isDarkMode = value;
+            });
+            print('Auto Resume: $isDarkMode');
+          },
+          activeColor: Colors.black,
+          inactiveThumbColor: Colors.blue,
+          inactiveTrackColor: Colors.blue.withOpacity(0.5),
         ),
       ],
     );
