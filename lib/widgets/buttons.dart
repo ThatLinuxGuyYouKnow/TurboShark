f83d13fcd4f2c85cc4e6f771e5d33522f1fb17ee
Emmanuels-code <arocket04@gmail.com>
2024-12-07 23:30:10 +0100

    feature: integrating minimal dark mode support to button

--- lib/widgets/buttons.dart
+++ lib/widgets/buttons.dart
@@ -2,15 +2,16 @@ import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:provider/provider.dart';
 import 'package:turbo_shark/models/downloadProvider.dart';
+import 'package:turbo_shark/user_preferences.dart';
 
 class DownloadPromptButton extends StatelessWidget {
   final Function onNewDownloadPressed;
 
-  const DownloadPromptButton({
+  DownloadPromptButton({
     Key? key,
     required this.onNewDownloadPressed,
   }) : super(key: key);
-
+  final UserPreferences userPreferences = UserPreferences();
   @override
   Widget build(BuildContext context) {
     return LayoutBuilder(
@@ -19,56 +20,60 @@ class DownloadPromptButton extends StatelessWidget {
         double buttonWidth = constraints.maxWidth > 600 ? 200 : 150;
         double fontSize = constraints.maxWidth > 600 ? 15 : 12;
 
-        return GestureDetector(
-          onTap: () => onNewDownloadPressed(),
-          child: Container(
-            height: 50,
-            width: buttonWidth,
-            decoration: BoxDecoration(
-              gradient: LinearGradient(
-                colors: [
-                  Colors.blue.shade300,
-                  Colors.blue.shade400,
-                ],
-                begin: Alignment.topLeft,
-                end: Alignment.bottomRight,
-              ),
-              borderRadius: BorderRadius.circular(15),
-              boxShadow: [
-                BoxShadow(
-                  color: Colors.blue.shade200.withOpacity(0.5),
-                  spreadRadius: 1,
-                  blurRadius: 5,
-                  offset: const Offset(0, 3),
-                ),
-              ],
-            ),
-            child: Center(
-              child: Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 10),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    const Icon(
-                      Icons.add,
-                      color: Colors.white,
-                      size: 20,
+        return FutureBuilder(
+            future: userPreferences.getTheme(),
+            builder: (BuildContext, snapshot) {
+              return GestureDetector(
+                onTap: () => onNewDownloadPressed(),
+                child: Container(
+                  height: 50,
+                  width: buttonWidth,
+                  decoration: BoxDecoration(
+                    gradient: LinearGradient(
+                      colors: [
+                        Colors.blue.shade300,
+                        Colors.blue.shade400,
+                      ],
+                      begin: Alignment.topLeft,
+                      end: Alignment.bottomRight,
                     ),
-                    const SizedBox(width: 5),
-                    Text(
-                      'New Download',
-                      style: GoogleFonts.ubuntu(
-                        color: Colors.white,
-                        fontSize: fontSize,
-                        fontWeight: FontWeight.w500,
+                    borderRadius: BorderRadius.circular(15),
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.blue.shade200.withOpacity(0.5),
+                        spreadRadius: 1,
+                        blurRadius: 5,
+                        offset: const Offset(0, 3),
+                      ),
+                    ],
+                  ),
+                  child: Center(
+                    child: Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 10),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          const Icon(
+                            Icons.add,
+                            color: Colors.white,
+                            size: 20,
+                          ),
+                          const SizedBox(width: 5),
+                          Text(
+                            'New Download',
+                            style: GoogleFonts.ubuntu(
+                              color: Colors.white,
+                              fontSize: fontSize,
+                              fontWeight: FontWeight.w500,
+                            ),
+                          ),
+                        ],
                       ),
                     ),
-                  ],
+                  ),
                 ),
-              ),
-            ),
-          ),
-        );
+              );
+            });
       },
     );
   }
