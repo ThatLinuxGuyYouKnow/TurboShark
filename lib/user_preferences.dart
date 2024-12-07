f0894c7fed80b3724f4595fd7211c97b2ef492b1
Emmanuels-code <arocket04@gmail.com>
2024-12-07 22:28:12 +0100

    feature: integrated methods for holding user settings

--- lib/user_preferences.dart
+++ lib/user_preferences.dart
@@ -1,6 +1,12 @@
 import 'package:get_storage/get_storage.dart';
 
 class UserPreferences {
+  getTheme() async {
+    await GetStorage.init('user-data');
+    final box = GetStorage();
+    return box.read('dark-mode') ?? false;
+  }
+
   updateTheme() async {
     await GetStorage.init('user-data');
     final box = GetStorage();
