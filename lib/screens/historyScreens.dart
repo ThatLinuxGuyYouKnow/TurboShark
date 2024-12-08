0b27a9147fab87535f142541144ecc98049f655c
Emmanuels-code <arocket04@gmail.com>
2024-12-08 15:17:05 +0100

    feature: created history screen

---
+++ lib/screens/historyScreens.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import 'package:turbo_shark/widgets/appbar.dart';
+
+class HistoryScreens extends StatelessWidget {
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: CustomAppbar(textContent: 'History'),
+    );
+  }
+}
