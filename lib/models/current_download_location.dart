b60fe72534efd19d2798fc691673fb3f5f0c48cb
Emmanuels-code <arocket04@gmail.com>
2024-12-09 14:47:03 +0100

    feature: modifying ChangeNotifier for download location

--- lib/models/current_download_location.dart
+++ lib/models/current_download_location.dart
@@ -1,9 +1,21 @@
 import 'package:flutter/material.dart';
 import 'package:path_provider/path_provider.dart';
+import 'dart:io';
 
 class CurrentDownloadLocationProvider extends ChangeNotifier {
+  String? _currentDownloadLocation;
+
   String get downloadLocation =>
-      getCurrentDonwloadLocation() ?? _defaultDownloadLocation;
-  String _defaultDownloadLocation = getDownloadsDirectory().toString();
-  String? getCurrentDonwloadLocation() {}
+      _currentDownloadLocation ?? _defaultDownloadLocation;
+
+  final String _defaultDownloadLocation = getDownloadsDirectory().toString();
+
+  void setCurrentDownloadLocation(String newLocation) {
+    _currentDownloadLocation = newLocation;
+    notifyListeners();
+  }
+
+  String? getCurrentDonwloadLocation() {
+    return _currentDownloadLocation;
+  }
 }
