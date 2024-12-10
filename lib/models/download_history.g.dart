16a01a15f196fb14be621c506e4889b134cb99e0
Emmanuels-code <arocket04@gmail.com>
2024-12-10 11:00:13 +0100

    fix: re-ran build runner with new hive constraints

--- lib/models/download_history.g.dart
+++ lib/models/download_history.g.dart
@@ -22,13 +22,15 @@ class DownloadHistoryAdapter extends TypeAdapter<DownloadHistory> {
       filePath: fields[2] as String,
       progress: fields[3] as double,
       state: fields[4] as String,
+      size: fields[5] as int,
+      time: fields[6] as DateTime,
     );
   }
 
   @override
   void write(BinaryWriter writer, DownloadHistory obj) {
     writer
-      ..writeByte(5)
+      ..writeByte(7)
       ..writeByte(0)
       ..write(obj.id)
       ..writeByte(1)
@@ -38,7 +40,11 @@ class DownloadHistoryAdapter extends TypeAdapter<DownloadHistory> {
       ..writeByte(3)
       ..write(obj.progress)
       ..writeByte(4)
-      ..write(obj.state);
+      ..write(obj.state)
+      ..writeByte(5)
+      ..write(obj.size)
+      ..writeByte(6)
+      ..write(obj.time);
   }
 
   @override
