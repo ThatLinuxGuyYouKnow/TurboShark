5ab2ba7175617cf1cb583a7a6e2d1692d173badd
Emmanuels-code <arocket04@gmail.com>
2024-12-12 23:12:48 +0100

    feature: fit user preferred conc. download count into dropdown

--- lib/user_preferences.dart
+++ lib/user_preferences.dart
@@ -1,42 +1,32 @@
 import 'package:get_storage/get_storage.dart';
 
 class UserPreferences {
-  final box = GetStorage();
+  final GetStorage box = GetStorage('user-data');
 
   Future<bool> getTheme() async {
-    await GetStorage.init('user-data');
-
     final output = box.read('dark-mode');
 
     return output is bool ? output : false;
   }
 
   Future<void> updateTheme({required bool setToDarkMode}) async {
-    await GetStorage.init('user-data');
-
     await box.write('dark-mode', setToDarkMode);
   }
 
   setUserPreferredDownloadLocation({required String location}) async {
-    await GetStorage.init('user-data');
-
     await box.write('download-location', location);
   }
 
   Future<String?> getUserPreferredDownloadLocation() async {
-    await GetStorage.init('user-data');
-
     return box.read('download-location');
   }
 
   setUserPreferredConcurrentDownloads(
       {required int concurrentDownloads}) async {
-    await GetStorage.init('user-data');
     await box.write('concurrent-downloads', concurrentDownloads);
   }
 
   Future<int> getUserPreferredConcurrentDownloads() async {
-    await GetStorage.init('user-data');
     return box.read('concurrent-downloads');
   }
 }
